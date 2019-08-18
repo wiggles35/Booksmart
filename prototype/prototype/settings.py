563b8b5fc506e8adc78cde88cec581065bba24a4
wiggles35 <rwiggles@nd.edu>
2019-08-17 21:29:26 -0400

    undoes postgres push

--- prototype/prototype/settings.py
+++ prototype/prototype/settings.py
@@ -75,20 +75,11 @@ WSGI_APPLICATION = 'prototype.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    """
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    """
-    'default': {
-        'ENGINE': 'django.db.backends.psycopg2',
-        'NAME': 'booksmart_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Booksmart$',
-        'HOST': '',
-        'PORT': '5432',
-    }
+
 }
 
 
