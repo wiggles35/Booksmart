1dcded28832100d273e588b7d4ca1a4fbc8b1f5c
wiggles35 <rwiggles@nd.edu>
2019-08-24 13:27:41 -0400

    Production now uses POSTGRESQL Database

--- prototype/settings/local.py
+++ prototype/settings/local.py
@@ -2,3 +2,14 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 SECRET_KEY = '7456898oiuvbi876ir8tgbliygoubpiblk0987'
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+
+}
--- prototype/settings/production.py
+++ prototype/settings/production.py
@@ -1,3 +1,15 @@
 from .base import *
 INSTALLED_APPS += ("gunicorn",)
 SECRET_KEY = os.environ['SECRET_KEY']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd20si1a0vmjaov',
+        'USER': 'kfakoszmkchopf',
+        'PASSWORD': '80be5b694b4b4f2d1fbed91caddceee788026a487a92f6b5b13ba872803af7ca',
+        'HOST': 'ec2-107-22-238-217.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
