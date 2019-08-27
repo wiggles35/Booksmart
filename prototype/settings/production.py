dd0b8624640d7ef9912bc76144c037a6d097a2fe
wiggles35 <rwiggles@nd.edu>
2019-08-26 20:47:09 -0400

    Debugging with DEBUG=True

--- prototype/settings/production.py
+++ prototype/settings/production.py
@@ -1,6 +1,7 @@
 from .base import *
 INSTALLED_APPS += ("gunicorn",)
 SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = True
 #SESSION_COOKIE_SECURE = True
 #CSRF_COOKIE_SECURE = True
 #os.environ['HTTPS'] = "on"
