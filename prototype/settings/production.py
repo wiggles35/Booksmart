78783ea033519ee61118af7d59887bb50110de5c
wiggles35 <rwiggles@nd.edu>
2019-08-26 20:43:02 -0400

    comments out last edit

--- prototype/settings/production.py
+++ prototype/settings/production.py
@@ -1,9 +1,9 @@
 from .base import *
 INSTALLED_APPS += ("gunicorn",)
 SECRET_KEY = os.environ['SECRET_KEY']
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-os.environ['HTTPS'] = "on"
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
+#os.environ['HTTPS'] = "on"
 '''
 DATABASES = {
     'default': {
