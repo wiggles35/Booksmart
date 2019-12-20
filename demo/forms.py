f8d777a6202b5445146d20b7b12a355a7dcbc964
wiggles35 <rwiggles@nd.edu>
2019-12-20 11:39:45 -0500

    This temporarily fixes the ISBN issue

--- demo/forms.py
+++ demo/forms.py
@@ -7,7 +7,7 @@ class AddBookForm(forms.Form):
     #new_email = forms.EmailField(label='Please use your ND/SMC email')
     #new_seller_name = forms.CharField(label='Your Name')
     new_price = forms.IntegerField(label='Asking Price')
-    new_isbn = forms.IntegerField(label='ISBN Number')
+    new_isbn = forms.IntegerField(label='ISBN Number(IF YOU ONLY HAVE AN ISBN-13, PUT "1". IF ISBN-10 PUT THE ISBN)')
     new_class_name = forms.CharField(label='Class Name')
     new_book_name = forms.CharField(label='Book Title')
     new_promise_box = forms.BooleanField(label="I promise to update my listing if it sells")
